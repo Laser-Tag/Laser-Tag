07c97eb72d297ac48d6af90718e2ecd5b97eaea6
Pukinens <pjtube@gmail.com>
2019-02-02 21:53:09 +0200

    Reload sound added

--- Elements Testing/Test_sound_by_one_wire/Test_sound_by_one_wire.ino
+++ Elements Testing/Test_sound_by_one_wire/Test_sound_by_one_wire.ino
@@ -11,23 +11,34 @@
 #define LT_TRIGGER_MODE_SEMI 0
 #define LT_TRIGGER_MODE_AUTO 1
 
+#define LT_RELOAD_PIN	4
+
+Bounce reloadBouncer = Bounce();
 Bounce triggerBouncer = Bounce();
 WT588D wt588d(WT588D_RST, WT588D_SDA, WT588D_BUSY);
 
 void setup()
-	{
-		triggerBouncer.attach(LT_TRIGGER_PIN, INPUT);
-		triggerBouncer.interval(10);
-		wt588d.begin();
-		pinMode(4, OUTPUT);
+{
+	reloadBouncer.attach(LT_RELOAD_PIN, INPUT);
+	reloadBouncer.interval(10);
+
+	triggerBouncer.attach(LT_TRIGGER_PIN, INPUT);
+	triggerBouncer.interval(10);
+
+	wt588d.begin();
 }
 
 void loop()
 {
-	digitalWrite(4, !digitalRead(4));
-	triggerBouncer.update();
+
 	if (!wt588d.isBusy())
 	{
+		reloadBouncer.update();
+		if (reloadBouncer.risingEdge())
+		{
+			wt588d.play(2);
+		}
+		triggerBouncer.update();
 		if (triggerBouncer.risingEdge())
 		{
 			if (digitalRead(LT_TRIGGER_MODE) == LT_TRIGGER_MODE_AUTO)
