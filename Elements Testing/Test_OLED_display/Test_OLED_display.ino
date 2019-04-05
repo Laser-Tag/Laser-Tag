45d94c795a535e6ea062bb8157679b085baac82e
Pukinens <pjtube@gmail.com>
2019-04-05 23:59:34 +0300

    Experiments with large display

--- Elements Testing/Test_OLED_display/Test_OLED_display.ino
+++ Elements Testing/Test_OLED_display/Test_OLED_display.ino
@@ -1,9 +1,12 @@
+#include <u8x8.h>
+#include <U8g2lib.h>
+#include <u8g2.h>
 #include <U8x8lib.h>
 #include <Bounce2.h>
 
 
 #define LT_TRIGGER_PIN	5
-#define LT_TRIGGER_MODE	6
+#define LT_TRIGGER_MODE_PIN	6
 
 #define LT_TRIGGER_MODE_SEMI 0
 #define LT_TRIGGER_MODE_AUTO 1
@@ -12,7 +15,27 @@
 
 Bounce reloadBouncer = Bounce();
 Bounce triggerBouncer = Bounce();
-U8X8_SSD1306_128X32_UNIVISION_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);   // Adafruit ESP8266/32u4/ARM Boards + FeatherWing OLED
+//U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
+//U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // OLEDs without Reset of the Display
+//U8X8_SSD1306_128X64_ALT0_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE); 	      // same as the NONAME variant, but may solve the "every 2nd line skipped" problem
+
+
+U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(SCL, SDA);
+//U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(U8X8_PIN_NONE);
+//U8X8_SSD1306_128X64_NONAME_2ND_HW_I2C u8x8(U8X8_PIN_NONE);
+
+//U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8x8(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+//U8X8_SSD1306_128X64_VCOMH0_SW_I2C u8x8(SCL, SDA);
+//U8X8_SSD1306_128X64_VCOMH0_HW_I2C u8x8(U8X8_PIN_NONE);
+//U8X8_SSD1306_128X64_VCOMH0_2ND_HW_I2C u8x8(U8X8_PIN_NONE);
+
+//U8X8_SSD1306_128X64_ALT0_SW_I2C u8x8(SCL, SDA);
+//U8X8_SSD1306_128X64_ALT0_HW_I2C u8x8(U8X8_PIN_NONE);
+//U8X8_SSD1306_128X64_ALT0_2ND_HW_I2C u8x8(U8X8_PIN_NONE);
+
+
+//U8X8_SSD1306_128X32_UNIVISION_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);   // Adafruit ESP8266/32u4/ARM Boards + FeatherWing OLED
 //U8X8_SSD1306_128X32_UNIVISION_SW_I2C u8x8(/* clock=*/ A3, /* data=*/ A2, /* reset=*/ U8X8_PIN_NONE);   // Adafruit Feather ESP8266/32u4 Boards + FeatherWing OLED
 
 int clipCount = 3;
@@ -35,11 +58,16 @@ void redraw()
 
 void setup()
 {
+	//Serial.begin(9600);
+
 	reloadBouncer.attach(LT_RELOAD_PIN, INPUT);
 	reloadBouncer.interval(10);
 
 	triggerBouncer.attach(LT_TRIGGER_PIN, INPUT);
 	triggerBouncer.interval(10);
+
+	//Serial.println(u8x8.);
+	//u8x8.setBusClock(1000);
 
 	u8x8.begin();
 	u8x8.setFont(u8x8_font_8x13B_1x2_r);
@@ -61,7 +89,7 @@ void loop()
 	triggerBouncer.update();
 	if (triggerBouncer.risingEdge())
 	{
-		if (digitalRead(LT_TRIGGER_MODE) == LT_TRIGGER_MODE_AUTO)
+		if (digitalRead(LT_TRIGGER_MODE_PIN) == LT_TRIGGER_MODE_AUTO)
 		{
 			ammoCount = ammoCount - 4;
 		}
