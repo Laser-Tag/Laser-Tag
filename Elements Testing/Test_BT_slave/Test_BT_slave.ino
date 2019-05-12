fb23db3298b9f0a880b3737a3bc5583f2270419f
Pukinens <pjtube@gmail.com>
2019-05-12 19:54:08 +0300

    Redesign 1. Remove Software serial 2. Add status pin output

--- Elements Testing/Test_BT_slave/Test_BT_slave.ino
+++ Elements Testing/Test_BT_slave/Test_BT_slave.ino
@@ -1,61 +1,38 @@
-#include <SoftwareSerial.h>// import the serial library
+#define SERIAL_BAUD   38400
 
 #define LED_PIN 13
 #define BT_STATE_PIN 9
 
-SoftwareSerial btSerial(7, 8); // RX, TX
 int btData; // the data given from Computer
 bool btConnected = false;
 
 void setup()
 {
+	pinMode(LED_PIN, OUTPUT);
 	pinMode(BT_STATE_PIN, INPUT);
 
-	//? Hardware serial
-	Serial.begin(9600);
+	Serial.begin(SERIAL_BAUD);
+
 	Serial.println("Bluetooth On. Press 1 or 0 to switch LED.");
-
-	//? Software serial
-	while (!btConnected)
-	{
-		btConnected = digitalRead(BT_STATE_PIN) == HIGH;
-	}
-
-	Serial.println("HC-05 is now connected");
-	btSerial.begin(9600);
-	btSerial.println("Bluetooth On. Press 1 or 0 to switch LED.");
-	pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-	//? Hardware serial
 	if (Serial.available())
 	{
 		btData = Serial.read();
 		if (btData == '1')
 		{
 			digitalWrite(LED_PIN, 1);
-			Serial.println("LED ON!");
+			Serial.print("LED ON! ");
+			Serial.print("BT status pin = ");
+			Serial.println(digitalRead(BT_STATE_PIN));
 		}
 		if (btData == '0')
 		{
 			digitalWrite(LED_PIN, 0);
-			Serial.println("LED Off!");
+			Serial.print("LED Off!");
+			Serial.print("BT status pin = ");
+			Serial.println(digitalRead(BT_STATE_PIN));
 		}
 	}
-	//? Software serial
-	if (btSerial.available()) {
-		btData = btSerial.read();
-		if (btData == '1')
-		{
-			digitalWrite(LED_PIN, 1);
-			btSerial.println("LED ON!");
-		}
-		if (btData == '0')
-		{
-			digitalWrite(LED_PIN, 0);
-			btSerial.println("LED Off!");
-		}
-	}
-	delay(100);
 }
