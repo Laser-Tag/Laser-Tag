1fcd875989c3e613a0706d651708717fe959306e
Pukinens <pjtube@gmail.com>
2019-05-14 03:46:40 +0300

    Add messaging functionality and minor tweaks 1. Serial port speed is in define 2. Reading print constants from programm memory 3. IR LED shine continuously when it is triggered in auto mode

--- Elements Testing/Test_complex_weapon/Test_complex_weapon.ino
+++ Elements Testing/Test_complex_weapon/Test_complex_weapon.ino
@@ -3,8 +3,11 @@
 #include <u8x8.h>
 #include <U8x8lib.h>
 #include <IRremote.h>
+#include <CmdMessenger.h>
 
 // Prefix "LT_" means LasetTag :-)
+
+#define SERIAL_BAUD   38400
 
 #define LT_SOUND_SHOOT		0
 #define LT_SOUND_LONGSHOOT	1
@@ -40,7 +43,16 @@ U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(U8X8_PIN_NONE);
 #define LT_BATTERY_LEVEL_PIN	14
 
 #define IR_PIN 3 // InfraRed LED pin
-IRsend irsend;
+IRsend irSend;
+
+CmdMessenger cmdMessenger = CmdMessenger(Serial);
+
+enum
+{
+	kStatus, // Command to report status
+	kSetLed,
+	kSetAmmo
+};
 
 
 int weaponID = 10;
@@ -54,8 +66,7 @@ int clipCount = 99;
 
 void setup()
 {
-	Serial.begin(38400); // BT module is configured for speed 38400
-
+	Serial.begin(SERIAL_BAUD); // BT module is configured for speed 38400
 	wt588d.begin();
 
 	u8x8.begin();
@@ -66,65 +77,80 @@ void setup()
 	pinMode(LT_MOTOR_PIN, OUTPUT);
 	pinMode(IR_PIN, OUTPUT);
 
+
+	cmdMessenger.printLfCr();
+
+	cmdMessenger.attach(OnUnknownCommand);
+	cmdMessenger.attach(kSetLed, OnSetLed);
+	cmdMessenger.attach(kSetAmmo, OnSetAmmo);
+
+	pinMode(LED_BUILTIN, OUTPUT);
+
+
 }
 
 void loop()
 {
-	//digitalWrite(LT_MOTOR_PIN, wt588d.isBusy());
+	cmdMessenger.feedinSerialData();
 
 	if (busyBouncer.update())
 	{
 		if (busyBouncer.fallingEdge())
 		{
 			digitalWrite(LT_MOTOR_PIN, HIGH);
-			digitalWrite(IR_PIN, HIGH);
 		}
 		if (busyBouncer.risingEdge())
 		{
 			digitalWrite(LT_MOTOR_PIN, LOW);
-			digitalWrite(IR_PIN, LOW);
 		}
 	}
 
-	//if (!wt588d.isBusy())
+	if (triggerBouncer.update())
 	{
-		if (triggerBouncer.update())
+		if (triggerBouncer.risingEdge())
 		{
-			if (triggerBouncer.risingEdge())
+			if (ammoCount > 0)
 			{
-				if (ammoCount > 0)
+				if (digitalRead(LT_TRIGGER_MODE_PIN) == LT_TRIGGER_MODE_SEMI)
 				{
-					if (digitalRead(LT_TRIGGER_MODE_PIN) == LT_TRIGGER_MODE_SEMI)
-					{
-						wt588d.play(LT_SOUND_SHOOT);
-						ammoCount = ammoCount - 1;
-					}
-					if (digitalRead(LT_TRIGGER_MODE_PIN) == LT_TRIGGER_MODE_AUTO)
-					{
-						wt588d.play(LT_SOUND_LONGSHOOT);
-						ammoCount = ammoCount - 4;
-					}
+					wt588d.play(LT_SOUND_SHOOT);
+					ammoCount = ammoCount - 1;
+					irSend.sendNEC(0xFF629D, 32);
 				}
-				else
+				if (digitalRead(LT_TRIGGER_MODE_PIN) == LT_TRIGGER_MODE_AUTO)
 				{
-					wt588d.play(LT_SOUND_EMPTYCLIP);
+					//wt588d.play(LT_SOUND_LONGSHOOT);
+					//ammoCount = ammoCount - 4;
+					digitalWrite(IR_PIN, HIGH);
 				}
-				ammoCount = (ammoCount < 0) ? 0 : ammoCount;
-				updateDisplay();
+			}
+			else
+			{
+				wt588d.play(LT_SOUND_EMPTYCLIP);
+			}
+			ammoCount = (ammoCount < 0) ? 0 : ammoCount;
+			updateDisplay();
+		}
+		if (triggerBouncer.fallingEdge())
+		{
+			if (digitalRead(LT_TRIGGER_MODE_PIN) == LT_TRIGGER_MODE_AUTO)
+			{
+				digitalWrite(IR_PIN, LOW);
 			}
 		}
-		if (reloadBouncer.update())
+	}
+
+	if (reloadBouncer.update())
+	{
+		if (reloadBouncer.risingEdge())
 		{
-			if (reloadBouncer.risingEdge())
+			if (clipCount > 0)
 			{
-				if (clipCount > 0)
-				{
-					wt588d.play(LT_SOUND_RELOAD);
-					ammoCount = ammoInClip;
-					clipCount = clipCount - 1;
-				}
-				updateDisplay();
+				wt588d.play(LT_SOUND_RELOAD);
+				ammoCount = ammoInClip;
+				clipCount = clipCount - 1;
 			}
+			updateDisplay();
 		}
 	}
 }
@@ -133,15 +159,39 @@ void updateDisplay()
 {
 	//u8x8.clear();
 	u8x8.setCursor(0, 0);
-	u8x8.print("Team: RED");
+	u8x8.print(F("Team: RED"));
 	u8x8.setCursor(0, 2);
-	u8x8.print("Health:");
+	u8x8.print(F("Health:"));
 	u8x8.print(lifeCount);
 	u8x8.println("%");
 	u8x8.setCursor(0, 4);
-	u8x8.print("Ammo:");
+	u8x8.print(F("Ammo:"));
 	u8x8.println(ammoCount);
 	u8x8.setCursor(0, 6);
-	u8x8.print("Clip:");
+	u8x8.print(F("Clip:"));
 	u8x8.println(clipCount);
+}
+
+void OnUnknownCommand()
+{
+	u8x8.setCursor(0, 2);
+	u8x8.print(F("Unknown         "));
+	u8x8.setCursor(0, 4);
+	u8x8.print(F("command         "));
+	u8x8.setCursor(0, 6);
+	u8x8.print(F("received        "));
+}
+
+void OnSetLed()
+{
+	digitalWrite(LED_BUILTIN, cmdMessenger.readBoolArg());
+	cmdMessenger.sendCmd(kStatus, digitalRead(LED_BUILTIN));
+	//digitalWrite(IR_PIN, digitalRead(LED_BUILTIN));
+}
+
+void OnSetAmmo()
+{
+	int dataType = cmdMessenger.readInt16Arg();
+	ammoCount = dataType;
+	updateDisplay();
 }
